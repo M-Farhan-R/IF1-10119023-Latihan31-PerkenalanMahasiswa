d9d1bacfce5fa3d8d8a2b595b5ad2d303833eaea
Muhammad Farhan R <farhan.10119023@mahasiswa.unikom.ac.id>
2020-11-07 19:24:00 +0700

    Tugas 31 - Perkenalan Mahasiswa

---
+++ src/if1/pkg10119023/latihan31/perkenalanmahasiswa/Mahasiswa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119023.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author Muhammad Farhan R 
+ */
+public class Mahasiswa {
+    public String nim;
+    public String nama;
+    
+    public void perkenalanDiri(){
+        this.nama = nama;
+        this.nim = nim;
+        System.out.println("Hallo Everyone\nMy NIM is ".concat(nim));
+        System.out.println("My Name is ".concat(nama));
+    }
+}
