b2376ccd4b6835a5d2c1640604e0a9c3b27ec6c2
Muhammad Farhan R <farhan.10119023@mahasiswa.unikom.ac.id>
2020-11-07 19:21:18 +0700

    Tugas 31 - Perkenalan Mahasiswa

---
+++ src/if1/pkg10119023/latihan31/perkenalanmahasiswa/IF110119023Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package if1.pkg10119023.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author 
+ * NAMA              : Muhammad Farhan R 
+ * KELAS             : IF1
+ * NIM               : 10119023
+ * Deskripsi Program :
+ *
+ */
+public class IF110119023Latihan31PerkenalanMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa();
+        mhs.nama = "Muhammad Farhan R";
+        mhs.nim = "10119023";
+        mhs.perkenalanDiri();
+    }
+
+}
